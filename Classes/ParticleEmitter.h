53579b666c7a2da031996d5882798b9da51e21b2
Bill Schofield <bschofield@yahoo.com>
2010-08-11 19:28:30 -0500

    Bill/Tim - Copied ParticleEmitter concept from GLViewController (but did not yet remove the original code

---
+++ Classes/ParticleEmitter.h
@@ -0,0 +1,25 @@
+//
+//  ParticleEmitter.h
+//  Particles
+//
+//  Created by bschofil on 8/11/10.
+//  Copyright 2010 ThoughtWorks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PolarCoordinatePositionController.h"
+#import "GravityPositionController.h"
+#import "PositionColorController.h"
+
+
+@interface ParticleEmitter : NSObject {
+
+}
+const int numberOfParticles = 1024;
+Vertex3D* particles;
+Color3D* colors;
+NSMutableArray *controllers;
+
+@end
+
+
---
+++ Classes/ParticleEmitter.m
@@ -0,0 +1,46 @@
+//
+//  ParticleEmitter.m
+//  Particles
+//
+//  Created by bschofil on 8/11/10.
+//  Copyright 2010 ThoughtWorks. All rights reserved.
+//
+
+#import "ParticleEmitter.h"
+
+
+@implementation ParticleEmitter
+
+- (id) init
+{
+	particles = malloc(sizeof(Vertex3D) * numberOfParticles);
+	colors = malloc(sizeof(Color3D) * numberOfParticles);
+	controllers = [[NSMutableArray alloc] init];
+	[controllers addObject: [[GravityPositionController alloc] init:particles :numberOfParticles]];
+	[controllers addObject: [[PolarCoordinatePositionController alloc] init:particles :numberOfParticles]];
+	[controllers addObject: [[PositionColorController alloc] init:colors :particles :numberOfParticles]];
+	return self;
+}
+
+- (void) update
+{
+	for (id controller in controllers) 
+	{
+		[controller update];
+	}	
+}
+
+- (void)dealloc 
+{
+	for (id controller in [controllers reverseObjectEnumerator])
+	{
+		free(controller);
+	}
+	free(controllers);
+	free(colors);
+	free(particles);
+
+    [super dealloc];
+}
+
+@end
